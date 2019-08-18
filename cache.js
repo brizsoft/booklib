b7f8bd0a0ac1375f3e8f19c27e4e72a54ef9a83f
brizsoft <igor7@brizsoft.com>
2019-08-18 17:40:44 +0300

    1

---
+++ cache.js
@@ -0,0 +1,40 @@
+const redis = require('redis')
+const client = redis.createClient()
+
+client.on("error", function(err) {
+  console.log("Error " + err);
+});
+
+const {promisify} = require('util');
+const getRedisAsync = promisify(client.get).bind(client);
+const setRedisAsync = promisify(client.set).bind(client);
+const flushdbAsync = promisify(client.flushdb).bind(client);
+
+async function cacheMiddleware(ctx, next) {
+  if (ctx.request.method === "GET") {
+    let data = await getCache(ctx.request.url);
+    if (data) {
+      ctx.body = data;
+    } else {
+      await next();
+      setCache(ctx.request.url, ctx.body);
+    }
+  } else {
+    clearCache();
+    await next();
+  }
+}
+
+async function getCache(key) {
+  return JSON.parse(await getRedisAsync(key));
+}
+
+async function setCache(key, val) {
+  await setRedisAsync(key, JSON.stringify(val));
+}
+
+async function clearCache(key) {
+  await flushdbAsync();
+}
+
+module.exports = cacheMiddleware;
